79b2c46a85b3094f3ebd877dddc5c13e895555e8
CrisRiffo <crisstofer67@gmail.com>
2026-02-07 15:26:33 -0300

    Fix service

--- weather_app_frontend/src/services/weather-chile-service.ts
+++ weather_app_frontend/src/services/weather-chile-service.ts
@@ -1,18 +1,17 @@
-import { WeatherApiResponse } from "@models/api-response/weather-chile-api-response";
+import { Weather } from "@models/class/weather-chile-class";
 import { WeatherDTO } from "@models/dto/weather-chile-dto";
 import { WeatherMapper } from "@models/mapper/weather-chile-mapper";
 
-export async function getWeather(): Promise<WeatherDTO[]> {
+export async function getWeather(): Promise<Weather[]> {
 	const response = await fetch(`${import.meta.env.VITE_API_URL}`, {
 			method: 'GET',
 			headers: {
-				"Content-Type": "application/json",
+				"Content-Type": `${import.meta.env.VITE_PARSE_HEADER}`
 			}
 		}); 
-	const dataResponse = await response.json();
-	const data = dataResponse.data;
-	return data.map((apiResponse: WeatherApiResponse) => {
-		const domain = WeatherMapper.fromApiToDTO(apiResponse);
+	const dataResponse: WeatherDTO[] = await response.json();
+	return dataResponse.map((data) => {
+		const domain = WeatherMapper.fromDTOToDomain(data);
 		return WeatherMapper.fromDTOToDomain(domain);
 	});
 }
