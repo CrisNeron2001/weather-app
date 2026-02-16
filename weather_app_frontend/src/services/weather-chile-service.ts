b62c08f054da7b0b6667726031e9c4f6e074a55e
CrisRiffo <crisstofer67@gmail.com>
2026-02-16 00:35:49 -0300

    fix service

--- weather_app_frontend/src/services/weather-chile-service.ts
+++ weather_app_frontend/src/services/weather-chile-service.ts
@@ -1,17 +1,30 @@
 import { Weather } from "@models/class/weather-chile-class";
-import { WeatherDTO } from "@models/dto/weather-chile-dto";
-import { WeatherMapper } from "@models/mapper/weather-chile-mapper";
+import { WeatherApiResponse } from "@models/respose-api/weather-chile-api";
 
 export async function getWeather(): Promise<Weather[]> {
-	const response = await fetch(`${import.meta.env.VITE_API_URL}`, {
+	const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}`, {
 			method: 'GET',
 			headers: {
-				"Content-Type": `${import.meta.env.VITE_PARSE_HEADER}`
+				"accept": 'application/json',
 			}
 		}); 
-	const dataResponse: WeatherDTO[] = await response.json();
-	return dataResponse.map((data) => {
-		const domain = WeatherMapper.fromDTOToDomain(data);
-		return WeatherMapper.fromDTOToDomain(domain);
-	});
+
+	const text = await response.text();
+
+	let json: WeatherApiResponse;
+
+	try {
+		json = JSON.parse(text);
+	} catch (error) {
+		throw console.log("Error al transformar el texto a json: ", error);
+	}
+
+	if (!response.ok) {
+		throw new Error(json?.status ?? "Error en la peticion")
+	}
+
+	if (!json?.data || !Array.isArray(json.data)) {
+		throw new Error("Formato de respuesta inválido");
+	}
+	return json.data;
 }
